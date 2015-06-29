eb144909f4dff8634005afd9fd1cd0911b305cd4
aperez <aperez@biit-solutions.com>
2015-06-29 12:33:39 +0200

    Modified config reader to read only a file with the name settings.cong form the same folder than the jar

--- src/main/java/com/biit/birt/configuration/BirtConfigurationReader.java
+++ src/main/java/com/biit/birt/configuration/BirtConfigurationReader.java
@@ -1,9 +1,17 @@
 package com.biit.birt.configuration;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.biit.logger.BiitCommonLogger;
+import com.biit.utils.configuration.ConfigurationReader;
+import com.biit.utils.configuration.PropertiesSourceFile;
 import com.biit.utils.configuration.SystemVariablePropertiesSourceFile;
 import com.biit.utils.configuration.exception.PropertyNotFoundException;
 
-public class BirtConfigurationReader extends JarConfigurationReader {
+public class BirtConfigurationReader extends ConfigurationReader {
+	protected static final String SETTINGS_FILE = "settings.conf";
 	private static final String SYSTEM_VARIABLE = "BIRT_CONFIG";
 	private static final String WEB_SERVICE_USER_ID = "web.service.rest.user";
 	private static final String WEB_SERVICE_PASS_ID = "web.service.rest.password";
@@ -11,8 +19,14 @@ public class BirtConfigurationReader extends JarConfigurationReader {
 	private static BirtConfigurationReader instance;
 
 	public BirtConfigurationReader() {
-		super(BirtConfigurationReader.class);
-
+		BiitCommonLogger.debug(this.getClass(), "Loading default settings file...");
+		// Load settings as file.
+		String settingsFile = getJarFolder() + "/" + SETTINGS_FILE;
+		BiitCommonLogger.debug(this.getClass(), "Searching for configuration file in '" + settingsFile + "'.");
+		if (fileExists(settingsFile)) {
+			addPropertiesSource(new PropertiesSourceFile(getJarFolder(), SETTINGS_FILE));
+			BiitCommonLogger.debug(this.getClass(), "Found configuration file '" + settingsFile + "'!");
+		}
 		// Load folder in system variables.
 		addPropertiesSource(new SystemVariablePropertiesSourceFile(SYSTEM_VARIABLE, SETTINGS_FILE));
 	}
@@ -45,12 +59,53 @@ public class BirtConfigurationReader extends JarConfigurationReader {
 		}
 		return null;
 	}
-	
+
 	public String getWebServiceUser() {
 		return getPropertyValue(WEB_SERVICE_USER_ID);
 	}
-	
+
 	public String getWebServicePass() {
 		return getPropertyValue(WEB_SERVICE_PASS_ID);
+	}
+
+	private URL getJarUrl() {
+		URL url = this.getClass().getResource('/' + this.getClass().getName().replace('.', '/') + ".class");
+		if (url == null) {
+			return null;
+		}
+		// Remove class inside JAR file (i.e. jar:file:///outer.jar!/file.class)
+		String packetPath = url.getPath();
+		if (packetPath.contains("!")) {
+			packetPath = packetPath.substring(0, packetPath.indexOf("!"));
+		}
+
+		packetPath = packetPath.replace("jar:", "");
+		try {
+			url = new URL(packetPath);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			BiitCommonLogger.errorMessageNotification(this.getClass(), e);
+		}
+
+		return url;
+	}
+
+	protected String getJarFolder() {
+		URL settingsUrl = getJarUrl();
+		return settingsUrl.getPath().substring(0, settingsUrl.getPath().lastIndexOf('/'));
+	}
+
+	protected String getJarName() {
+		URL settingsUrl = getJarUrl();
+		if (settingsUrl == null || !settingsUrl.getPath().contains(".jar")) {
+			return null;
+		}
+		return settingsUrl.getPath().substring(settingsUrl.getPath().lastIndexOf('/') + 1,
+				settingsUrl.getPath().length() - ".jar".length());
+	}
+
+	private boolean fileExists(String filePathString) {
+		File f = new File(filePathString);
+		return (f.exists() && !f.isDirectory());
 	}
 }

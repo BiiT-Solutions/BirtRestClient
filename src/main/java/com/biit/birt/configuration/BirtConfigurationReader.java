69898bee0ec9438f2b0668466fb956fc260e9498
amelcon <amelcon@biit-solutions.com>
2016-09-07 15:13:00 +0200

    I upload what I have forgot! :-(

--- src/main/java/com/biit/birt/configuration/BirtConfigurationReader.java
+++ src/main/java/com/biit/birt/configuration/BirtConfigurationReader.java
@@ -8,7 +8,7 @@ import com.biit.logger.BiitCommonLogger;
 import com.biit.utils.configuration.ConfigurationReader;
 import com.biit.utils.configuration.PropertiesSourceFile;
 import com.biit.utils.configuration.SystemVariablePropertiesSourceFile;
-import com.biit.utils.configuration.exception.PropertyNotFoundException;
+import com.biit.utils.configuration.exceptions.PropertyNotFoundException;
 
 public class BirtConfigurationReader extends ConfigurationReader {
 	protected static final String SETTINGS_FILE = "settings.conf";
--- src/main/java/com/biit/birt/rest/RestClient.java
+++ src/main/java/com/biit/birt/rest/RestClient.java
@@ -15,14 +15,18 @@ import com.biit.birt.configuration.BirtConfigurationReader;
 public class RestClient {
 
 	public static String callRestServiceJson(String targetPath, String path, String json) {
-		return get(targetPath, path, MediaType.APPLICATION_JSON, json);
+		return post(targetPath, path, MediaType.APPLICATION_JSON, json);
 	}
 
 	public static String callRestServiceXml(String targetPath, String path, String json) {
-		return get(targetPath, path, MediaType.APPLICATION_XML, json);
+		return post(targetPath, path, MediaType.APPLICATION_XML, json);
 	}
-
-	private static String get(String target, String path, String requestType, String json) {
+	
+	public static String callRestServiceImage(String targetPath, String path){
+		return get(targetPath,path,"image/jpg");
+	}
+	
+	private static String post(String target, String path, String requestType, String json) {
 		boolean ssl = target.startsWith("https");
 		String responseString = null;
 
@@ -38,6 +42,29 @@ public class RestClient {
 			response = ClientBuilder.newBuilder().build().target(UriBuilder.fromUri(target).build()).path(path)
 					.register(authenticationFeature).request(requestType)
 					.post(Entity.entity(json, MediaType.APPLICATION_JSON));
+		}
+		if (response.getStatusInfo().toString().equals(Response.Status.OK.toString())) {
+			responseString = response.readEntity(String.class);
+		}
+		return responseString;
+	}
+	
+	private static String get(String target, String path, String requestType) {
+		boolean ssl = target.startsWith("https");
+		String responseString = null;
+
+		HttpAuthenticationFeature authenticationFeature = HttpAuthenticationFeature.basic(BirtConfigurationReader
+				.getInstance().getWebServiceUser(), BirtConfigurationReader.getInstance().getWebServicePass());
+		Response response = null;
+		if (ssl) {
+			SSLContext sslContext = SslConfigurator.newInstance(true).createSSLContext();
+			response = ClientBuilder.newBuilder().sslContext(sslContext).build()
+					.target(UriBuilder.fromUri(target).build()).path(path).register(authenticationFeature)
+					.request(requestType).get();
+		} else {
+			response = ClientBuilder.newBuilder().build().target(UriBuilder.fromUri(target).build()).path(path)
+					.register(authenticationFeature).request(requestType)
+					.get();
 		}
 		if (response.getStatusInfo().toString().equals(Response.Status.OK.toString())) {
 			responseString = response.readEntity(String.class);
